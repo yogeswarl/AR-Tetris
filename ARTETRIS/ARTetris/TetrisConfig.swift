818f4b98398473b77c0320da2a326aaff94fd81a
Yogeswar <yogi2198@gmail.com>
2019-05-02 16:44:53 +0530

    Add files via upload

---
+++ ARTETRIS/ARTetris/TetrisConfig.swift
@@ -0,0 +1,11 @@
+
+/** Tetris configuration: width and height of the well */
+struct TetrisConfig {
+	
+	static let standard: TetrisConfig = TetrisConfig(width: 10, height: 20)
+	
+	let width: Int
+	let height: Int
+	
+}
+
